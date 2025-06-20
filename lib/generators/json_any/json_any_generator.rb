3cc03d23a2e1a88ba35367395025069fd1a32810
michaeldimmitt <michaelgdimmitt@gmail.com>
2025-06-20 09:43:43 -0400

    working state

--- lib/generators/json_any/json_any_generator.rb
+++ lib/generators/json_any/json_any_generator.rb
@@ -1,3 +1,28 @@
-class JsonAnyGenerator < Rails::Generators::NamedBase
+class JsonanyGenerator < Rails::Generators::Base
   source_root File.expand_path("templates", __dir__)
+
+  argument :json_file, type: :string
+  attr_reader :json_config, :model_name, :model_name_plural, :model_name_underscore
+
+  def read_file
+    data = File.read(json_file)
+    @json_config = JSON.parse(data)
+    @model_name = @json_config["model_name"]
+    @model_name_underscore = @model_name.underscore
+    @model_name_plural = @model_name_underscore.pluralize
+    @command = @json_config['command']
+  end
+
+  def create_generator
+    querystr = ''
+    @json_config["columns"].each do |column|
+      querystr << "#{column["name"]}:#{column["type"]} "
+    end
+    generate "#{@command} #{@model_name} #{querystr}"
+    # Example output: generate "scaffold User email:string first_name:string last_name:string"
+
+    # Unused alternative method:
+    # hook_for :scaffold_controller, in: :rails, as: :controller
+  end
+
 end
